8ddd4427ee3d9034e55256971327661603a5eb5c
hasemasa1984 <hasemasa0719@gmail.com>
2022-09-15 22:00:28 +0900

    修正5

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -2,7 +2,7 @@ class PrototypesController < ApplicationController
   before_action :set_prototype, only: [:edit, :show]
   before_action :authenticate_user!, except: [:index, :show, :edit, :update]
   before_action :contributor_confirmation, only: [:edit, :update, :destroy]
-  before_action :move_to_index, except: [:index, :show]
+  #before_action :move_to_index, except: [:index, :show]
 
 
   def index
