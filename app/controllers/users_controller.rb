e62a48fc12d91664484bf18f342d7cdb41b7b85a
hasemasa1984 <hasemasa0719@gmail.com>
2022-09-19 20:56:47 +0900

    修正8

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,7 @@
 class UsersController < ApplicationController
-  #before_action :authenticate_user!, except: [:show]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def show
-    #user = User.find(params[:id])
-    #@nickname = user.nickname
-    #@prototypes = user.prototypes
     @user = User.find(params[:id])
     #if 
       #redirect to user_path(@prototype)
