4ea777b48a8e28bd5480980f5762e334171454da
hasemasa1984 <hasemasa0719@gmail.com>
2022-11-15 08:54:34 +0900

    ラストコミット

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,5 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    #if 
-      #redirect to user_path(@prototype)
-    #else
-      #@prototype = @comment.prototype
-      #@comments = @prototype.comments
-      #render "prototypes/show"
-    #end
   end
 end
